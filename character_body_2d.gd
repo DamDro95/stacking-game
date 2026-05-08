336f2a78f04bdc6c686dab397a86677e2cf1057e
Adam Pacheco <adampacheco98@hotmail.com>
2026-05-08 14:31:48 -0400

    Added Spawning Function with Accessible Timers + Kill zone

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
+const SPEED = 200.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
