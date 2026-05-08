acb095797b1c2da2daaba078ad7d32bce1a911b3
Damian <damian.drozd@hotmail.com>
2026-05-08 11:53:24 -0400

    Added falling animation to stackable

--- Scripts/stackable_object.gd
+++ Scripts/stackable_object.gd
@@ -1,10 +1,9 @@
-extends Node2D
+extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.gravity_scale = 0
-	self.inear_velocity = Vector2(0, 300) # Falls down at 300 pixels per second
-	
+	pass # Replace with function body.
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.y += 100 * delta
