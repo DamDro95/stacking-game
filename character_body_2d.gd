f154defa57479c9d84e3d95237e2274d0143781b
Damian <damian.drozd@hotmail.com>
2026-05-08 20:58:15 -0400

    Added stack building

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,6 +5,8 @@ const JUMP_VELOCITY = -400.0
 
 var stack: Array[Area2D] = []
 
+@onready var stack_marker = $Marker2D
+
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -17,6 +19,33 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	area.reparent(self)
+	if area.get_meta("is_stacked", false):
+		return
+		
+	area.set_meta("is_stacked", true)
+	area.get_node("CollisionShape2D").set_deferred("moitorable", false)
+	area.get_node("CollisionShape2D").set_deferred("monitorable", false)
+	call_deferred("_stack", area)
+
+func _stack(area: Area2D) -> void:
+	# Double check it hasn't been deleted or freed
+	if not is_instance_valid(area):
+		return
+		
+	# Disable collisions now that we are outside the "flushing" lock
+	area.monitoring = false
+	area.monitorable = false
+	
+	# Reparent
+	area.reparent(self, false)
+	area.set_process(false)
+	
+	# Position and Stack logic
+	area.position = stack_marker.position
+	
+	var sprite = area.get_node("Sprite2D")
+	var item_height = abs(sprite.get_rect().size.y * sprite.scale.y)
+	
+	stack_marker.position.y -= item_height
 	stack.append(area)
-	print(stack)
+	$Area2D.position = stack_marker.position
