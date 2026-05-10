97ff2f0b99e758f926bc39ed7ad35aaef47119d3
Damian <damian.drozd@hotmail.com>
2026-05-10 15:31:49 -0400

    Finalized game loop logic

--- Scripts/gameplay/recipe_manager.gd
+++ Scripts/gameplay/recipe_manager.gd
@@ -22,29 +22,40 @@ func _init():
 func new_recipe() -> void:
 	# Randomize the seed (crucial so you don't get the same "random" results every time)
 	randomize() 
-	items.shuffle()
+	body_items.shuffle()
+	head_items.shuffle()
 	
-	var new_items = items.slice(0, 4)
+	var number_of_body_parts = randi_range(1, 3)
+	
+	var new_items = body_items.slice(0, number_of_body_parts)
 	recipe = {}
 	for item in new_items:
-		recipe[item] = randi_range(1, 2)
+		recipe[item] = randi_range(1, 6)
+	
+	var head_item = head_items[0]
+	recipe[head_item] = 1
 
 func stack_calculation(area:Area2D) -> void:
 	var anim_sprit = area.get_node('AnimatedSprite2D')
 	var anim_name = anim_sprit.animation
 	
-	if anim_name in recipe and recipe[anim_name] > 0:
-		recipe[anim_name] -= 1;
-		valid_item_stacked.emit(anim_name, recipe)
-		AudioController.play_stack_success()
-	else:
+	var is_body_recipe_complete = recipe.keys().all(func(key):
+		if "head" in key:
+			return true
+		return recipe[key] == 0
+	)
+	
+	if (anim_name not in recipe
+	or (anim_name in recipe and recipe[anim_name] < 1)
+	or ("head" in anim_name and not is_body_recipe_complete)):
 		invalid_item_stacked.emit()
 		AudioController.play_stack_fail()
 		return
+	
+	recipe[anim_name] -= 1;
+	valid_item_stacked.emit(anim_name, recipe)
+	AudioController.play_stack_success()
 		
-	# Check if recipe is complete
-	var is_complete = recipe.values().all(func(v): return v == 0)
-	if is_complete: #and "head" in anim_name:
+	var is_recipe_complete = recipe.values().all(func(v): return v == 0)
+	if is_recipe_complete and "head" in anim_name:
 		recipe_completed.emit()
-	#elif not is_complete and "head" in anim_name:
-		#invalid_item_stacked.emit()
